18c5d95a92fc033286688421be8b662721d919a4
saki komai <55061969+saki789@users.noreply.github.com>
2023-08-27 10:15:10 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const fieldsets = document.querySelectorAll('.fieldset-container');
-    const progressBar = document.getElementById('progress-bar');
-    const previousButton = document.getElementById('previous-button');
-    const nextButton = document.getElementById('next-button');
+  const fieldsets = document.querySelectorAll('fieldset');
+  const progressBar = document.querySelector('.progress-bar');
+  const nextButtons = document.querySelectorAll('.next-btn');
+  const prevButtons = document.querySelectorAll('.prev-btn');
+  const submitButton = document.querySelector('.submit-btn');
+  let currentFieldset = 0;
 
-    let currentFieldset = 0;
+  updateProgressBar();
+
+  // Show the current fieldset and hide the others
+  function showFieldset(index) {
+    fieldsets.forEach((fieldset, i) => {
+      fieldset.style.display = i === index ? 'block' : 'none';
+    });
+    currentFieldset = index;
     updateProgressBar();
+  }
 
-    function updateProgressBar() {
-        const progress = (currentFieldset / (fieldsets.length - 1)) * 100;
-        progressBar.style.width = `${progress}%`;
+  // Update progress bar based on current fieldset
+  function updateProgressBar() {
+    const progress = ((currentFieldset + 1) / fieldsets.length) * 100;
+    progressBar.style.width = `${progress}%`;
+  }
+
+  // Show next fieldset
+  function nextFieldset() {
+    if (currentFieldset < fieldsets.length - 1) {
+      showFieldset(currentFieldset + 1);
     }
+  }
 
-    function showFieldset(index) {
-        fieldsets.forEach((fieldset, i) => {
-            fieldset.style.display = i === index ? 'block' : 'none';
-        });
-        updateProgressBar();
+  // Show previous fieldset
+  function prevFieldset() {
+    if (currentFieldset > 0) {
+      showFieldset(currentFieldset - 1);
     }
+  }
 
-    function showNextFieldset() {
-        if (currentFieldset < fieldsets.length - 1) {
-            currentFieldset++;
-            showFieldset(currentFieldset);
-        }
+  // Add event listeners to Next and Previous buttons
+  nextButtons.forEach(button => {
+    button.addEventListener('click', nextFieldset);
+  });
+
+  prevButtons.forEach(button => {
+    button.addEventListener('click', prevFieldset);
+  });
+
+  // Validate form before submitting
+  submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Add your validation logic here
+    const isValid = validateForm();
+
+    if (isValid) {
+      // Submit the form
+      alert('Form submitted successfully!');
+      // You can also use AJAX to submit the form data to the server
     }
+  });
 
-    function showPreviousFieldset() {
-        if (currentFieldset > 0) {
-            currentFieldset--;
-            showFieldset(currentFieldset);
-        }
-    }
+  // Your validation logic
+  function validateForm() {
+    // Implement your validation checks here
+    // Return true if the form is valid, otherwise return false
+    return true;
+  }
 
-    showFieldset(currentFieldset);
-
-    nextButton.addEventListener('click', function () {
-        showNextFieldset();
-    });
-
-    previousButton.addEventListener('click', function () {
-        showPreviousFieldset();
-    });
+  // Auto-focus on the first input field of the first fieldset
+  const firstInput = fieldsets[0].querySelector('input, textarea');
+  if (firstInput) {
+    firstInput.focus();
+  }
 });
