374302db23eda58a28b3baee6e54e520a9466414
saki komai <55061969+saki789@users.noreply.github.com>
2023-08-27 17:01:01 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,75 @@ document.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById("registration-form");
   
   form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
     let isValid = true;
     
-    // Validation logic for each field
-    if (!validateName("first-name")) {
-      isValid = false;
-    }
-    // Repeat for other fields...
-    
-    if (!isValid) {
-      event.preventDefault(); // Prevent form submission
+    isValid = validateField("first-name", "This field is required.") && isValid;
+    isValid = validateField("middle-name", "") && isValid;
+    isValid = validateField("last-name", "This field is required.") && isValid;
+    isValid = validateField("dob", "This field is required.") && isValid;
+    isValid = validateField("address", "This field is required.") && isValid;
+    isValid = validateField("phone", "This field is required.") && isValid;
+    isValid = validateField("passport", "This field is required.") && isValid;
+    isValid = validateField("tin", "This field is required.") && isValid;
+
+    isValid = validateField("employer-name", "This field is required.") && isValid;
+    isValid = validateField("job-title", "This field is required.") && isValid;
+    isValid = validateField("employer-address", "This field is required.") && isValid;
+    isValid = validateField("employer-phone", "This field is required.") && isValid;
+
+    isValid = validateField("institution-name", "This field is required.") && isValid;
+    isValid = validateField("start-year", "This field is required.") && isValid;
+    isValid = validateField("end-year", "This field is required.") && isValid;
+    isValid = validateField("qualification", "This field is required.") && isValid;
+
+    isValid = validateField("bank-name", "This field is required.") && isValid;
+    isValid = validateField("account-holder", "This field is required.") && isValid;
+    isValid = validateField("account-number", "This field is required.") && isValid;
+
+    if (isValid) {
+      // Prepare and send form data to server
+      const formData = new FormData(form);
+
+      fetch("server-url", {
+        method: "POST",
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          // Display success message or redirect to success page
+        } else {
+          // Display error message
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
     }
   });
-  
-  function validateName(fieldId) {
-    const field = document.getElementById(fieldId);
-    const value = field.value.trim();
-    
-    if (value === "") {
-      showError(field, "This field is required.");
-      return false;
-    }
-    
-    return true;
-  }
-  
-  // Define similar validation functions for other fields
 });
 
-function showError(field, errorMessage) {
-  const errorElement = document.createElement("div");
-  errorElement.className = "error-message";
-  errorElement.textContent = errorMessage;
+function validateField(fieldId, errorMessage) {
+  const field = document.getElementById(fieldId);
+  const value = field.value.trim();
   
-  field.parentNode.appendChild(errorElement);
+  const errorElement = field.nextElementSibling;
+  
+  if (value === "") {
+    showError(errorElement, errorMessage);
+    return false;
+  }
+  
+  hideError(errorElement);
+  return true;
+}
+
+function showError(errorElement, errorMessage) {
+  errorElement.textContent = errorMessage;
+}
+
+function hideError(errorElement) {
+  errorElement.textContent = "";
 }
