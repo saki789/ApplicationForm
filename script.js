47d845ef95b9052af2e8a10a497febbb00af8a0c
saki komai <55061969+saki789@users.noreply.github.com>
2023-08-27 16:59:39 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-function showPage(pageNumber) {
-    document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
-    document.querySelector('#page' + pageNumber).style.display = 'block';
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("registration-form");
+  
+  form.addEventListener("submit", function(event) {
+    let isValid = true;
+    
+    // Validation logic for each field
+    if (!validateName("first-name")) {
+      isValid = false;
+    }
+    // Repeat for other fields...
+    
+    if (!isValid) {
+      event.preventDefault(); // Prevent form submission
+    }
+  });
+  
+  function validateName(fieldId) {
+    const field = document.getElementById(fieldId);
+    const value = field.value.trim();
+    
+    if (value === "") {
+      showError(field, "This field is required.");
+      return false;
+    }
+    
+    return true;
+  }
+  
+  // Define similar validation functions for other fields
+});
+
+function showError(field, errorMessage) {
+  const errorElement = document.createElement("div");
+  errorElement.className = "error-message";
+  errorElement.textContent = errorMessage;
+  
+  field.parentNode.appendChild(errorElement);
 }
